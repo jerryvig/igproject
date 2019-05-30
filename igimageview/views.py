b1dbcae3e31670ba5697bc317e632c9a147e10df
Jerry Vigil <jerry.vigil@mktneutral.com>
2019-05-29 19:12:12 -0500

    First commit of the Instagram Image View Project.

---
+++ igimageview/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+# from igimageview.models import blah, blah, blah
+
+# Create your views here.
+
+def index(request):
+    """View function for the homepage of the site."""
+
+    # some context object to pass to the template renderer here.
+    context = {
+        'natalia_jimenez': 'creo creo creo en mi',
+    }
+
+    return render(request, 'index.html', context=context)
