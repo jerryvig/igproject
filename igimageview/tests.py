ca266c3b1cc255b0ea22a4dfd1cbaa51b37a5681
Jerry Vigil <jerry.vigil@mktneutral.com>
2019-06-24 17:17:51 -0500

    updates

--- igimageview/tests.py
+++ igimageview/tests.py
@@ -1,4 +1,21 @@
 from django.test import TestCase
 
 # Create your tests here.
+class MyTestClass(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        print('In MyTestClass.setUpTestData() which is a class method.')
+        pass
 
+    def setUp(self):
+        print('in MyTestClass.setUp()')
+        pass
+
+    def tearDown(self):
+        print('in MyTestClass.tearDown()')
+
+    def test_something_that_will_pass(self):
+        self.assertFalse(False)
+
+    def test_something_that_will_fail(self):
+        self.assertTrue(True)
