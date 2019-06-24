a562fe0e3ea73661244c43fe17843e9bc3cff8f8
Jerry Vigil <jerry.vigil@mktneutral.com>
2019-06-24 12:11:31 -0500

    updates

--- igimageview/models.py
+++ igimageview/models.py
@@ -1,8 +1,22 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
 class IGPost(models.Model):
-    """Model representing and Instagram post."""
+    """Model representing an Instagram post."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this instagram post')
+
+    url = models.CharField(max_length=512, help_text='Enter IG Post URL')
+
+    poster_name = models.CharField(max_length=512, help_text='Enter the Instagram Posters name.')
+
+    type = ()
+
+    def __str__(self):
+        """String representation of the IG Post. Using the post's URL for now."""
+        return self.url
+
+class IGImage(models.Model):
+    """Model representing an Instagram image."""
     pass
-
-
--- igimageview/views.py
+++ igimageview/views.py
@@ -6,10 +6,14 @@ from django.shortcuts import render
 def index(request):
     """View function for the homepage of the site."""
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     # some context object to pass to the template renderer here.
     context = {
         'natalia_jimenez': 'creo creo creo en mi',
         'music_data': 'music data here',
+        'num_visits': num_visits,
     }
 
     return render(request, 'index.html', context=context)
