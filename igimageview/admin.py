e9a24bd51255a76ace222fc349c44d42ceea3638
Jerry Vigil <jerry.vigil@mktneutral.com>
2019-06-24 14:54:38 -0500

    updates

--- igimageview/admin.py
+++ igimageview/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from igimageview.models import IGPost
+from igimageview.models import IGImage
 
 # Register your models here.
+admin.site.register(IGPost)
+admin.site.register(IGImage)
